96e1bcf3f62e1688d8686e293703bcd87b27f7f6
Clemons11 <mattclems11@gmail.com>
2017-05-04 17:27:35 -0400

    Led Disk
    Led Disk Project

---
+++ Lab6/LAB6/SRC/eGFX/Sprites/Sprites_16BPP_565.h
@@ -0,0 +1,29 @@
+#include "stdint.h" 
+#include "eGFX.h" 
+
+#ifndef _SPRITES_16BPP_565_H
+#define _SPRITES_16BPP_565_H
+
+////***************************************************************************
+////                         Sprite_16BPP_565_light
+////***************************************************************************
+
+
+__attribute__ ((section ("ExtFlashSection"))) extern const eGFX_ImagePlane 	Sprite_16BPP_565_light;
+
+////***************************************************************************
+////                         Sprite_16BPP_565_pie
+////***************************************************************************
+
+
+__attribute__ ((section ("ExtFlashSection"))) extern const eGFX_ImagePlane 	Sprite_16BPP_565_pie;
+
+////***************************************************************************
+////                         Sprite_16BPP_565_stop
+////***************************************************************************
+
+
+__attribute__ ((section ("ExtFlashSection"))) extern const eGFX_ImagePlane 	Sprite_16BPP_565_stop;
+
+#endif
+
